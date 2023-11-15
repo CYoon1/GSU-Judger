268119fdee964aaae5b352ec18736289bdcd035f
j0zart <johnthoang1684@gmail.com>
2023-11-15 17:59:11 -0500

    changed add project button size
    changed add project button size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,11 +68,16 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    testImplementation("com.google.truth:truth:1.0.1")
+    androidTestImplementation("com.google.truth:truth:1.0.1")
 }
